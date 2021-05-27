ccd08954c83796679b3868ae2f49c9b28cb62aad
unknown <louiezx1@gmail.com>
2021-05-26 21:39:36 -0400

    Second Push

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,11 +10,22 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+       anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+       anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+
+        anim.SetBool("moving", (Mathf.Abs(movement.x) > 0) || (Mathf.Abs(movement.y) > 0));
     }
     void FixedUpdate()
     {
